89a943471f54b8d91ddc5a02988fe49d9d986f3d
1506493280zsx-max <1506493280zsx@gmail.com>
2026-06-29 11:42:15 +0900

    fix: remove duplicate payment title, shrink logo size

--- components/PaymentMethods.tsx
+++ components/PaymentMethods.tsx
@@ -4,17 +4,14 @@ import Image from "next/image";
 export default function PaymentMethods() {
   return (
     <div>
-      <div style={{ fontSize: 10, fontWeight: 700, color: "#CCEEEE", letterSpacing: 1.5, marginBottom: 12 }}>
-        安心のお支払い方法&ensp;/&ensp;PAYMENT METHODS
-      </div>
       <Image
         src="/payment/payment-methods-transparent.png"
         alt="Payment Methods"
-        width={480}
-        height={220}
+        width={604}
+        height={120}
         style={{
           width: "100%",
-          maxWidth: "480px",
+          maxWidth: "360px",
           height: "auto",
           objectFit: "contain",
           display: "block",
